119220c24ce458c796939e85fc98d5c8ddd6874f
Rama Astra <ramaastra333@gmail.com>
2024-03-25 23:40:24 +0800

    feat: add database connection

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+require('dotenv').config();
+const db = require('./db');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+app.get('/tasks', async (req, res) => {
+  try {
+    const { rows } = await db.query('SELECT * FROM tasks');
+    res.status(200).json(rows);
+  } catch (error) {
+    res.status(500).json({ status: 'fail', error: error.message });
+  }
+});
 
 app.get('/', (req, res) => {
   res.status(200).json({
