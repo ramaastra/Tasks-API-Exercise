98ba793d381ee19309d7671b69932c05bfaca7b8
Rama Astra <ramaastra333@gmail.com>
2024-04-04 00:01:29 +0800

    feat: setup cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 require('dotenv').config();
 const db = require('./db');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/tasks', async (req, res) => {
